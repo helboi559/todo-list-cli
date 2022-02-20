51feb8a6e665626f28d84b443602922eabd32347
Juan Ramirez <rami8224.2007@gmail.com>
2022-02-20 16:00:28 -0500

    added line comments

--- to-do.js
+++ to-do.js
@@ -19,6 +19,7 @@ while(items.length >= 0) {
     } else {
         //log running list
         console.log(`You have ${items.length} to-do's`) 
+        //iterate to log by line
         for(let j = 0 ; j < items.length ;j++) {
             if (complete[j] === false) {
                 let line = `${itemNums[j]} [Incomplete] ${items[j]}`
@@ -31,19 +32,23 @@ while(items.length >= 0) {
         }
     
     }
-    //ask for options
+    //ask for add ir complete task
     let question1 = Number(prompt('[1]Add or [2]Complete a task? '))
     //if ADD
     if (question1 === 1) {
         console.log('Creating a new item...')
+        //prompt for task
         let question2 = prompt('What is this task called?: ')
-        //add items to individual array
+        //add items to individual arrays
         items.push(question2)
         complete.push(false)
         itemNums.push(itemNum)
+        //increase counter by one
         itemNum ++ ;
     } else if (question1 === 2) {
         console.log('Completing current item..')
+        //if we want to complete an item...
+        // log current status of list by looping
         for(let k = 0 ; k < items.length ;k++) {
             if (complete[k] === false) {
                 let line = `${itemNums[k]} [Incomplete] ${items[k]}`
@@ -58,12 +63,10 @@ while(items.length >= 0) {
         let toDo = Number(prompt('What task do you want to complete? '))
         //loop through all arrays and set as complete
          for(let i = 0 ; i < items.length ; i++) {
-            
             //if num matches number in array
             if (toDo === itemNums[i]) {
                 //change value to true in another array at same index
                 complete[i] = true
-
             }
             
         }
